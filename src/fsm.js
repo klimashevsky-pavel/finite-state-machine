ea056333c54f19bf4d50abda066a80140e72420f
klimashevsky-pavel <31858309+klimashevsky-pavel@users.noreply.github.com>
2017-09-17 14:57:23 +0300

    Update fsm.js

--- src/fsm.js
+++ src/fsm.js
@@ -1,60 +1,126 @@
 class FSM {
-    /**
-     * Creates new FSM instance.
-     * @param config
-     */
-    constructor(config) {}
 
-    /**
-     * Returns active state.
-     * @returns {String}
-     */
-    getState() {}
+    constructor(config) {
+        this.config = config;
+        this.config.state = this.config.initial;
+        this.config.prevstate = [];
+        this.config.count = 0;
+        this.config.undocount = 0;
+        this.triggercount = 0;
+        this.changestatecount = 0;
+}
 
-    /**
-     * Goes to specified state.
-     * @param state
-     */
-    changeState(state) {}
+    getState() {
+        return this.config.state;
 
-    /**
-     * Changes state according to event transition rules.
-     * @param event
-     */
-    trigger(event) {}
 
-    /**
-     * Resets FSM state to initial.
-     */
-    reset() {}
+    }
 
-    /**
-     * Returns an array of states for which there are specified event transition rules.
-     * Returns all states if argument is undefined.
-     * @param event
-     * @returns {Array}
-     */
-    getStates(event) {}
+    changeState(state) {
+        if(state in this.config.states){
+        this.config.prevstate[this.config.count] = this.config.state;
+        this.config.count = +this.config.count + 1;
+        this.config.state = state;
+        this.changestatecount ++;
 
-    /**
-     * Goes back to previous state.
-     * Returns false if undo is not available.
-     * @returns {Boolean}
-     */
-    undo() {}
+
+    }
+    else error(function() {
+    });
+
+    }
+
+    trigger(event) {
+    var key = this.config.state;
+        if(this.config.states[key].transitions[event]){
+            this.config.prevstate[this.config.count] = this.config.state;
+            this.config.count = +this.config.count + 1;
+            this.config.state =  this.config.states[key].transitions[event];
+            this.triggercount++;
+        }
+        else error(function() {
+
+        });
+
+    
+
+    }
+
+    reset() {
+        this.config.state = this.config.initial;
+
+    }
+
+    getStates(event) {
+        var array = [];
+        var i = 0;
+        if(event == null){
+        for(var key in this.config.states){
+            array[i] = key;
+            i++;
+        }
+        return array;
+    }
+
+    for(var key in this.config.states){
+        if(this.config.states[key].transitions[event]){
+         array[i] = key;
+        i++;   
+        }
+
+    }
+     return array;   
+    }
+
+    undo() {
+        if(!this.config.prevstate[0] ){
+            return false;
+        }
+        if(this.config.count < 1){
+            return false;
+        }
+        if ( this.config.count >= 1 ){
+        this.config.prevstate[this.config.count] = this.config.state;   
+        this.config.count = +this.config.count - 1;
+        this.config.state = this.config.prevstate[this.config.count];
+        this.config.undocount = +this.config.undocount + 1;
+        this.triggercount = 0;
+        this.changestatecount = 0;
+        return true;
+    }
+
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(!this.config.prevstate[0]  || this.config.undocount < 1 || 
+            this.triggercount > 0 || this.changestatecount > 0 ){
+            return false;
+        }
+        this.config.count = (+this.config.count + 1 ) ;
+        this.config.state = this.config.prevstate[this.config.count];
+        this.config.undocount = +this.config.undocount - 1;
+        this.triggercount = 0;
+        this.changestatecount = 0;
+        
+        return true;
+    
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.config.prevstate = [];
+        this.config.count = 0;
+    }
+
 }
+
 
 module.exports = FSM;
 
